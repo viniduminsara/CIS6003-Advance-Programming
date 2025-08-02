e1bcf00d1e43ad46eeadd0d231484113195e24a8
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-08-02 22:46:07 +0530

    add builder design pattern to customerDTO

--- src/main/java/dev/viniduminsara/pahanaedu/business/customer/dto/CustomerDTO.java
+++ src/main/java/dev/viniduminsara/pahanaedu/business/customer/dto/CustomerDTO.java
@@ -11,6 +11,16 @@ public class CustomerDTO {
     private LocalDate registrationDate;
     private String email;
 
+    private CustomerDTO(Builder builder) {
+        this.customerId = builder.customerId;
+        this.name = builder.name;
+        this.address = builder.address;
+        this.mobileNumber = builder.mobileNumber;
+        this.unitsConsumed = builder.unitsConsumed;
+        this.registrationDate = builder.registrationDate;
+        this.email = builder.email;
+    }
+
     public CustomerDTO(String customerId, String name, String address, String mobileNumber, int unitsConsumed, LocalDate registrationDate, String email) {
         this.customerId = customerId;
         this.name = name;
@@ -19,6 +29,56 @@ public class CustomerDTO {
         this.unitsConsumed = unitsConsumed;
         this.registrationDate = registrationDate;
         this.email = email;
+    }
+
+    //builder class
+    public static class Builder {
+        private String customerId;
+        private String name;
+        private String address;
+        private String mobileNumber;
+        private int unitsConsumed;
+        private LocalDate registrationDate;
+        private String email;
+
+        public Builder customerId(String customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder mobileNumber(String mobileNumber) {
+            this.mobileNumber = mobileNumber;
+            return this;
+        }
+
+        public Builder unitsConsumed(int unitsConsumed) {
+            this.unitsConsumed = unitsConsumed;
+            return this;
+        }
+
+        public Builder registrationDate(LocalDate registrationDate) {
+            this.registrationDate = registrationDate;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public CustomerDTO build() {
+            return new CustomerDTO(this);
+        }
     }
 
     public String getCustomerId() {
