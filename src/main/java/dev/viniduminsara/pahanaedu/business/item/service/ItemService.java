8fe9ce818fffa5a1615aba357646db77792b989e
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2025-08-05 23:29:33 +0530

    create and get all implementation in item service

---
+++ src/main/java/dev/viniduminsara/pahanaedu/business/item/service/ItemService.java
@@ -0,0 +1,18 @@
+package dev.viniduminsara.pahanaedu.business.item.service;
+
+import dev.viniduminsara.pahanaedu.business.item.dto.ItemDTO;
+
+import java.util.List;
+
+public interface ItemService {
+
+    void saveItem(ItemDTO item);
+
+    ItemDTO getItemByCode(String itemCode);
+
+    List<ItemDTO> getAllItems();
+
+    void updateItem(String itemCode, ItemDTO updatedItem);
+
+    void deleteItem(String itemCode);
+}
---
+++ src/main/java/dev/viniduminsara/pahanaedu/business/item/service/impl/ItemServiceImpl.java
@@ -0,0 +1,48 @@
+package dev.viniduminsara.pahanaedu.business.item.service.impl;
+
+import dev.viniduminsara.pahanaedu.business.item.dto.ItemDTO;
+import dev.viniduminsara.pahanaedu.business.item.mapper.ItemMapper;
+import dev.viniduminsara.pahanaedu.business.item.model.Item;
+import dev.viniduminsara.pahanaedu.business.item.service.ItemService;
+import dev.viniduminsara.pahanaedu.persistence.item.dao.ItemDAO;
+import dev.viniduminsara.pahanaedu.persistence.item.dao.impl.ItemDAOImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ItemServiceImpl implements ItemService {
+
+    ItemDAO itemDAO = new ItemDAOImpl();
+
+    @Override
+    public void saveItem(ItemDTO item) {
+        item.setItemCode(UUID.randomUUID().toString());
+        itemDAO.save(ItemMapper.toEntity(item));
+    }
+
+    @Override
+    public ItemDTO getItemByCode(String itemCode) {
+        return null;
+    }
+
+    @Override
+    public List<ItemDTO> getAllItems() {
+        List<ItemDTO> itemDTOS = new ArrayList<>();
+        List<Item> itemList = itemDAO.findAll();
+        for (Item item : itemList) {
+            itemDTOS.add(ItemMapper.toDTO(item));
+        }
+        return itemDTOS;
+    }
+
+    @Override
+    public void updateItem(String itemCode, ItemDTO updatedItem) {
+
+    }
+
+    @Override
+    public void deleteItem(String itemCode) {
+
+    }
+}
